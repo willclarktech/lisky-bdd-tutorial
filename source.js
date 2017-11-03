477be6d3e3612b27a2c109244e211e9e8202118a
willclarktech <willclarktech@users.noreply.github.com>
2017-11-03 15:18:30 +0100

    Update source code for unhappy path

--- source.js
+++ source.js
@@ -2,6 +2,7 @@ const wishHappyBirthday = (name, language) => {
 	switch (language) {
 		case 'English': return `Happy birthday, ${name}!`;
 		case 'Deutsch': return `Herzlichen Glückwunsch zum Geburtstag, ${name}!`;
+		default: throw new Error('Unsupported language');
 	}	
 };
 
