d53dc7337149eadec14ed60f67c6df6433c9c21a
willclarktech <willclarktech@users.noreply.github.com>
2017-11-03 14:26:58 +0100

    Enrich specification for happy paths

--- specification.js
+++ specification.js
@@ -1,9 +1,40 @@
+const given = require('./given');
 const when = require('./when');
 const then = require('./then');
 
 describe('wishHappyBirthday', () => {
-	describe('When wishHappyBirthday is called with the name and the language', () => {
-		beforeEach(when.wishHappyBirthdayIsCalledWithTheNameAndTheLanguage);
-		it('Then it should return "Happy birthday, Lisky!"', then.itShouldReturn);
+	describe('Given a name "Lisky"', () => {
+		beforeEach(given.aName);
+		describe('Given a language "English"', () => {
+			beforeEach(given.aLanguage);
+			describe('When wishHappyBirthday is called with the name and the language', () => {
+				beforeEach(when.wishHappyBirthdayIsCalledWithTheNameAndTheLanguage);
+				it('Then it should return "Happy birthday, Lisky!"', then.itShouldReturn);
+			});
+		});
+		describe('Given a language "Deutsch"', () => {
+			beforeEach(given.aLanguage);
+			describe('When wishHappyBirthday is called with the name and the language', () => {
+				beforeEach(when.wishHappyBirthdayIsCalledWithTheNameAndTheLanguage);
+				it('Then it should return "Herzlichen Glückwunsch zum Geburtstag, Lisky!"', then.itShouldReturn);
+			});
+		});
+	});
+	describe('Given a name "Satoshi"', () => {
+		beforeEach(given.aName);
+		describe('Given a language "English"', () => {
+			beforeEach(given.aLanguage);
+			describe('When wishHappyBirthday is called with the name and the language', () => {
+				beforeEach(when.wishHappyBirthdayIsCalledWithTheNameAndTheLanguage);
+				it('Then it should return "Happy birthday, Satoshi!"', then.itShouldReturn);
+			});
+		});
+		describe('Given a language "Deutsch"', () => {
+			beforeEach(given.aLanguage);
+			describe('When wishHappyBirthday is called with the name and the language', () => {
+				beforeEach(when.wishHappyBirthdayIsCalledWithTheNameAndTheLanguage);
+				it('Then it should return "Herzlichen Glückwunsch zum Geburtstag, Satoshi!"', then.itShouldReturn);
+			});
+		});
 	});
 });
