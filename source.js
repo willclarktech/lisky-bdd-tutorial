9cd8089ef67327d1b0b42fcf0c92c57acef31a32
willclarktech <willclarktech@users.noreply.github.com>
2017-11-03 14:49:12 +0100

    Pass happy path tests in source code

--- source.js
+++ source.js
@@ -1,3 +1,8 @@
-const wishHappyBirthday = () => 'Happy birthday, Lisky!';
+const wishHappyBirthday = (name, language) => {
+	switch (language) {
+		case 'English': return `Happy birthday, ${name}!`;
+		case 'Deutsch': return `Herzlichen Glückwunsch zum Geburtstag, ${name}!`;
+	}	
+};
 
 module.exports = wishHappyBirthday;
